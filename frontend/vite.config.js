f5188d8b978d4dc7ff6213dd5a00f663a599b602
rayanaouf <rayanaouf1512@gmail.com>
2026-04-26 13:02:14 +0100

    test

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,8 @@ import path from 'path'
 import { getProxyOptions } from 'frappe-ui/src/utils/vite-dev-server'
 import { webserver_port } from '../../../sites/common_site_config.json'
 
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
